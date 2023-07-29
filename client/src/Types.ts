5dc5f2032ff755bde0f21da8892e40f0f9e26ef0
Thiago <thiagolos1999@gmail.com>
2023-07-29 11:40:01 +0100

    feat: frontend types defined

--- client/src/Types.ts
+++ client/src/Types.ts
@@ -1,32 +1,62 @@
+export type External_Urls = {
+  "spotify": string
+}
+
 export type Image = {
   height: number;
   url: string;
   width: number;
 }
 
+export type Artist = {
+  spotify_id: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 export type Track = {
+  id: string;
+  spotify_id: string;
   name: string;
   release_date: Date;
   popularity: number;
   preview_url: string;
   artists: string[];
+  images: Image[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export type SpotifyData = {
   id: string;
+  spotify_id: string;
   popularity: number;
   followers: number;
   genres: string[];
   images: Image[];
   top_tracks: Track[];
-  external_urls: string[];
+  external_urls: External_Urls;
   href: string;
   type: string;
   uri: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-export type Artist = {
-  id: string;
-  name: string;
+export type ArtistAndSpotify = {
   spotify_id: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+  SpotifyData: SpotifyData[];
+}
+
+export type SpotifyAndTracks = {
+  spotify_id: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+  SpotifyData: SpotifyData[];
+  TopTracks: Track[];
 }
